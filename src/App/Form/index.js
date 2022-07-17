29aa27e4d617ea9056dc74a5a153cd0bae340dfe
Adrian Osak <osak.adrian.k@gmail.com>
2022-07-17 09:06:06 +0200

    added Form component with logic and basic syles

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,18 +1,33 @@
 import "./style.css";
+import { useState } from "react";
+import currencies from "../currencies";
+import Result from "./Result";
 
-const Form = () => (
-    <form className="form">
-        <fieldset className="form__fieldset">
+const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState(currencies[0].id);
+    const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    }
+
+
+    return (<form className="form" onSubmit={onSubmit}>
+        
             <legend className="form__legend">
                 Field marked with * is required
             </legend>
             <p className="form__paragraph">
-                <label className="form__label">
+                <label>
                     <span className="form__labelText">
                         Amount in zloty*:
                     </span>
                     <input
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
                         className="amount"
+                        placeholder="Type amount in zloty"
                         type="number"
                         min="0.01"
                         step="any"
@@ -25,24 +40,31 @@ const Form = () => (
                     <span className="form__labelText">
                         Convert to:
                     </span>
-                    <select className="currency" name="currency">
-                        <option value="EUR">Euro</option>
-                        <option value="DOL">US Dollar</option>
-                        <option value="GBP">British Pound</option>
-                        <option value="CHF">Swiss Frank</option>
+                    <select
+                        className="currency"
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
+                    >
+                        {currencies.map((currency => (
+                            <option
+                                key={currency.id}
+                                value={currency.id}
+                            >
+                                {currency.name}
+                            </option>
+                        )))}
                     </select>
                 </label>
             </p>
-        </fieldset>
+       
         <p className="form__paragraph">
             <button className="form__button">
                 Convert
             </button>
         </p>
-        <p>
-            <span className="result"></span>
-        </p>
-    </form>
-);
+        <Result result={result} />
+    </form >
+    )
+};
 
 export default Form;
