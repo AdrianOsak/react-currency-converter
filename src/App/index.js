52aa3c32b78bd4c02d22365aec9a579f6ffd8458
Adrian Osak <osak.adrian.k@gmail.com>
2022-07-17 09:07:50 +0200

    added Form component and logic to App component

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,32 @@
 import "./style.css";
 import Form from "./Form";
+import { useState } from "react";
+import currencies from "./currencies";
 
 function App() {
+
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ id }) => id === currency)
+      .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
   return (
     <div className="App">
       <header className="header">Polish zloty converter</header>
       <main className="main">
-        <Form />
+        <Form
+          result={result}
+          calculateResult={calculateResult}
+        />
       </main>
     </div>
   );
