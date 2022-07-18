61c9501f404dc8ad20bbcec6ca0f77ea898efd90
Adrian Osak <osak.adrian.k@gmail.com>
2022-07-18 11:13:34 +0200

    updated rates

--- src/App/currencies.js
+++ src/App/currencies.js
@@ -2,22 +2,22 @@ const currencies = [
     {
         name: "Euro",
         id: "EUR",
-        rate: 4.8337
+        rate: 4.8115
     },
     {
         name: "US Dollar",
         id: "DOL",
-        rate: 4.8284
+        rate: 4.7966
     },
     {
         name: "British Pound",
         id: "GBP",
-        rate: 5.7119
+        rate: 5.6739
     },
     {
         name: "Swiss Frank",
         id: "CHF",
-        rate: 4.9031
+        rate: 4.8930
     },
 ];
 
