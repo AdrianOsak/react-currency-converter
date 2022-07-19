7712f33d07a7800e74891a8eea8b82664d66874e
Adrian Osak <osak.adrian.k@gmail.com>
2022-07-19 08:51:32 +0200

    background-image chagned to background

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -5,7 +5,7 @@ const Result = ({ result }) => (
         {result !== undefined && (
             <>
                 {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-                {""}
+                {" "}
                 <strong>
                     {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
                 </strong>
